e09d1edd7e95e070005f20a3ed2f8a376cdd961e
Shimanto-Kumar07 <kshimanto70@gmail.com>
2025-03-30 03:09:27 +0600

    User Input
    How to take User Input in C++

---
+++ userInput.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int apples;
+    cout << "Enter the number of apples: ";
+    cin >> apples;
+    cout << "Number of apples " << apples << ".";
+}
