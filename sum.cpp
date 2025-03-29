35843e9b5535d5b7fedf9585ec0a39e4fc4fe2f6
Shimanto-Kumar07 <kshimanto70@gmail.com>
2025-03-30 03:10:10 +0600

    Sum
    Sum of Two Numbers

---
+++ sum.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a, b, sum;
+
+    cout <<"Enter first number: ";
+    cin >> a;
+    cout << "Enter second Number: ";
+    cin >> b;
+
+    sum = a + b;
+
+    cout << "Sum is " << sum << ".";
+
+    return 0;
+}
